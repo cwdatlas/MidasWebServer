b7d4fca414c153b778878270456bd30e27ce599c
cwdatlas <ascott@carroll.edu>
2023-01-30 16:21:24 -0700

    initial commit, built from spring boot project

---
+++ src/main/java/com/midaswebserver/midasweb/MidaswebApplication.java
@@ -0,0 +1,13 @@
+package com.midaswebserver.midasweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidaswebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidaswebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/midaswebserver/midasweb/MidaswebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midaswebserver.midasweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidaswebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
