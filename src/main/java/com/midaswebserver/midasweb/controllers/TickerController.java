6f4d739da67275593a567b6dc8972b306d175089
cwdatlas <ascott@carroll.edu>
2023-03-01 10:54:03 -0700

    finished basic ingestion and display feature

--- src/main/java/com/midaswebserver/midasweb/controllers/TickerController.java
+++ src/main/java/com/midaswebserver/midasweb/controllers/TickerController.java
@@ -8,22 +8,29 @@ import com.midaswebserver.midasweb.forms.UserForm;
 import com.midaswebserver.midasweb.services.TickerService;
 import com.midaswebserver.midasweb.services.TickerServiceImp;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class TickerController {
+    private static final Logger log = LoggerFactory.getLogger(TickerController.class);
 
     private TickerService tickerService = new TickerServiceImp();
 
     @GetMapping("/ticker/data")
-    public ResponseEntity getTickerData(@Valid @ModelAttribute StockDataRequestForm stockDataRequestForm, BindingResult result, RedirectAttributes attrs){
-
-        Ticker ticker = tickerService.getTimeSeriesInfo("IBM", Interval.SIXTY_MIN, OutputSize.COMPACT);
+    public ResponseEntity getTickerData(@Valid @ModelAttribute StockDataRequestForm stockDataRequestForm, BindingResult result){
+        if(result.hasErrors()){
+            log.debug("Found Errors: '{}'", result.getAllErrors());
+            return ResponseEntity.ok(result.getAllErrors());
+        }
+        Ticker ticker = tickerService.getTimeSeriesInfo(stockDataRequestForm.getTicker(), stockDataRequestForm.getInterval(), OutputSize.COMPACT);
         return ResponseEntity.ok(ticker);
     }
 }
