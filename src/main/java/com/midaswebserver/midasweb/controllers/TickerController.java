2c3facfd8bad69938311a1d4f773fc05dec8b87a
cwdatlas <ascott@carroll.edu>
2023-03-07 23:23:52 -0700

    ticker saving has been added to TickerController

--- src/main/java/com/midaswebserver/midasweb/controllers/TickerController.java
+++ src/main/java/com/midaswebserver/midasweb/controllers/TickerController.java
@@ -3,16 +3,23 @@ package com.midaswebserver.midasweb.controllers;
 import com.crazzyghost.alphavantage.parameters.OutputSize;
 import com.midaswebserver.midasweb.apiModels.Ticker;
 import com.midaswebserver.midasweb.forms.StockDataRequestForm;
+import com.midaswebserver.midasweb.models.User.Settings;
+import com.midaswebserver.midasweb.models.User.User;
 import com.midaswebserver.midasweb.services.TickerService;
 import com.midaswebserver.midasweb.services.TickerServiceImp;
+import com.midaswebserver.midasweb.services.UserService;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.Stack;
 
 /**
  * TickerController will work with stock data and related forms like the StockDataRequestForm
@@ -21,6 +28,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 public class TickerController {
     private static final Logger log = LoggerFactory.getLogger(TickerController.class);
     private TickerService tickerService = new TickerServiceImp();
+    @Autowired
+    private UserService userService;
 
     /**
      * GetTickerData takes a StockDataRequestForm and returns the equivalent stock data
@@ -31,12 +40,21 @@ public class TickerController {
      * @return json of converted ticker object
      */
     @GetMapping("/ticker/data")
-    public ResponseEntity getTickerData(@Valid @ModelAttribute StockDataRequestForm stockDataRequestForm, BindingResult result){
+    public ResponseEntity getTickerData(@Valid @ModelAttribute StockDataRequestForm stockDataRequestForm, BindingResult result, HttpSession session){
         if(result.hasErrors()){
             log.debug("Found Errors: '{}'", result.getAllErrors());
             return ResponseEntity.ok(result.getAllErrors());
         }
         Ticker ticker = tickerService.getTimeSeriesInfo(stockDataRequestForm.getTicker(), stockDataRequestForm.getInterval(), OutputSize.COMPACT);
+        //adds called ticker to tickers that have been called before
+        if(ticker.getMetaData().getSymbol()!=null) {
+            User user = userService.getUserByID(Long.parseLong(session.getId()));
+            Settings settings = user.getSettings();
+            Stack tickerStack = settings.getTickers();
+            tickerStack.push(ticker.getMetaData().getSymbol());
+            settings.setTickers(tickerStack);
+            user.setSettings(settings);
+        }
         return ResponseEntity.ok(ticker);
     }
 }
