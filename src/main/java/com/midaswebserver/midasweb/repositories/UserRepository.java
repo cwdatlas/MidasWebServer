c2766c7da28d350c04ce8e9e05ca3bc5dfe39ff0
cwdatlas <ascott@carroll.edu>
2023-03-24 10:38:22 -0600

    updated javadoc for repository, adding @Author, @since and @version

--- src/main/java/com/midaswebserver/midasweb/repositories/UserRepository.java
+++ src/main/java/com/midaswebserver/midasweb/repositories/UserRepository.java
@@ -4,7 +4,20 @@ import com.midaswebserver.midasweb.models.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
+/**
+ * @Author Aidan Scott
+ * @since 0.0.1
+ * @version 0.0.1
+ * Vehicle to interact with the user table within the database
+ * Through relations, other tables can be interacted with {@see Setting}
+ */
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    /**
+     * @Author SpringBoot
+     * @since 0.0.1
+     * Finds User by username while ignoring case
+     * @param username
+     * @return User
+     */
     List<User> findByUsernameIgnoreCase(String username);
 }
