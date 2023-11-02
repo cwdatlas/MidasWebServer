2aa4dadc427f490e0a1ab36caa93ad45b25a9169
cwdatlas <ascott@carroll.edu>
2023-11-02 10:16:34 -0600

    first commit of basic custom client and server exceptions

---
+++ src/main/java/com/midaswebserver/midasweb/exceptions/ApiClientException.java
@@ -0,0 +1,7 @@
+package com.midaswebserver.midasweb.exceptions;
+
+public class ApiClientException extends Exception{
+    public ApiClientException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/com/midaswebserver/midasweb/exceptions/ApiServerException.java
@@ -0,0 +1,7 @@
+package com.midaswebserver.midasweb.exceptions;
+
+public class ApiServerException extends Exception{
+    public ApiServerException(String message){
+        super(message);
+    }
+}
