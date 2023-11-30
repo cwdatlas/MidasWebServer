1499f958e2018384af3338c9375dede7eac93897
cwdatlas <ascott@carroll.edu>
2023-11-30 09:35:31 -0700

    Commit for merge from microserviceUserInput branch

--- src/main/java/com/midaswebserver/midasweb/controllers/UserPersonal.java
+++ src/main/java/com/midaswebserver/midasweb/controllers/UserPersonal.java
@@ -21,7 +21,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
